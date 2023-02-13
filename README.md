6f898f13285c342b95e77dc615e2de80ed209b50
Ed Summers <ehs@pobox.com>
2023-02-13 06:24:45 -0500

    simplified by removing wikidata and zip join

--- README.md
+++ README.md
@@ -1,29 +1,32 @@
 # College Soccer Schools
 
-My son is interested in playing soccer at College and I wanted to get a dataset
+My son is interested in playing soccer at college and I wanted to get a dataset
 of colleges and universities with their associated conference and division,
 average SAT score, acceptance rate and annual cost. Maybe there's a way to do
-this using existing tools but what better way to sublimate the stress by doing
-this myself? 
+this using existing tools but what better way to sublimate/repress the stress
+by turning this into a data problem. 
 
-I started by using https://wikitable2csv.ggor.de/ to download the tables for
-the following Wikipedia Pages:
+I started by using [wikitable2csv] to download the tables for the following Wikipedia Pages:
 
 - https://en.wikipedia.org/wiki/List_of_NCAA_Division_I_men%27s_soccer_programs
 - https://en.wikipedia.org/wiki/List_of_NCAA_Division_II_men%27s_soccer_programs
 - https://en.wikipedia.org/wiki/List_of_NCAA_Division_III_institutions
 
-Then I downloaded the most recent [College School Card Data](https://collegescorecard.ed.gov/data/) from 
+Then I downloaded the most recent [College School Card Data] from:
 
 - https://ed-public-download.app.cloud.gov/downloads/Most-Recent-Cohorts-Institution_09012022.zip
 
-The `convert.py` program will:
+All these files are in the `data` directory. Then I ran the `convert.py` program to:
 
-1. Merge and normalize the division CSVs
-2. Reconcile the school names against Wikidata
-3. Get additional information from Wikidata (postal code, student count, admission rate, location)
-4. Join the data College School Card data using the postal code to get the admission rate, cost-per-year, average SAT, and admission rate.
-5. The `missing-wikidata.csv` contains rows for schools that weren't reconciled against Wikidata
-6. The `missing-postalcode.csv` files contains rows for Wikidata entities that lacked a postal code
-7. Writes out the resulting data to `data.csv`
+1. Merge and normalize the division CSVs.
+2. Join the data College School Card data, to add the admission rate, cost-per-year, average SAT, and geo-location.
+3. Write out the resulting data to `schools.csv`.
+
+You can see that there is quite a bit of schools (10%) that didn't get joined due to very different school names. The natural key I used was to capitalize the name, remove all punctuation and then sort the letters. Other approaches would be welcome!
+
+The `new-england.csv` subset was created by extracting New England schools (plus New York) for a trip we were planning to visit a few schools.
+
+[wikitable2csv]: https://wikitable2csv.ggor.de/ 
+[College School Card Data]: https://collegescorecard.ed.gov/data/
+
 
--- convert.py
+++ convert.py
@@ -1,32 +1,28 @@
 #!/usr/bin/env python3
 
-import time
+import re
 import pandas
-import requests
-import reconciler
 
 def main():
     df = get_divisions()
-    df = reconcile(df)
-    df = augment_wikidata(df)
     df = augment_doe(df)
-    df.to_csv('data.csv', index=False)
+    df.to_csv('schools.csv', index=False)
 
 def get_divisions():
     # Division 1
-    d1 = pandas.read_csv('List_of_NCAA_Division_I_men%27s_soccer_programs_1.csv')
+    d1 = pandas.read_csv('data/List_of_NCAA_Division_I_men%27s_soccer_programs_1.csv')
     d1 = d1[['Institution','Location','State', 'Conference']]
     d1.columns = ['School', 'City', 'State', 'Conference']
     d1['Division'] = 'D1'
 
     # Division 2
-    d2 = pandas.read_csv('List_of_NCAA_Division_II_men%27s_soccer_programs_1.csv')
+    d2 = pandas.read_csv('data/List_of_NCAA_Division_II_men%27s_soccer_programs_1.csv')
     d2 = d2[['School', 'City', 'State/Province', 'Conference']]
     d2.columns = ['School', 'City', 'State', 'Conference']
     d2['Division'] = 'D2'
 
     # Division 3
-    d3 = pandas.read_csv('List_of_NCAA_Division_III_institutions_1.csv')
+    d3 = pandas.read_csv('data/List_of_NCAA_Division_III_institutions_1.csv')
     d3 = d3[['School', 'City', 'State', 'Conference']]
     d3['Division'] = 'D3'
 
@@ -39,74 +35,24 @@ def get_divisions():
 
     return df
 
-def reconcile(df):
-    # reconcile against Wikidata entities of type higher education institution
-    recon = reconciler.reconcile(df['School'], type_id='Q38723')
-    recon = recon[['id', 'input_value']]
-
-    # merge the results, and drop/rename columns
-    df = df.merge(recon, left_on='School', right_on='input_value', how='left')
-    df = df.drop(['input_value'], axis=1)
-    df = df.rename({'id': 'WikidataID'}, axis=1)
-    df.to_csv('data.csv', index=False)
-
-    # track wikidata misses
-    missing = df[df['WikidataID'].isnull()]
-    missing.to_csv('missing-wikidata.csv', index=False)
-    df = df[~df['WikidataID'].isnull()]
-
-    return df
-
-def augment_wikidata(df):
-    extra = pandas.DataFrame.from_records(df['WikidataID'].map(get_wikidata))
-    df = df.merge(extra, on='WikidataID')
-    df.to_csv('extra.csv', index=False)
-
-    # track wikidata entities that lack postal code
-    missing_zip = df[df['PostalCode'].isnull()]
-    missing_zip.to_csv('missing-postalcode.csv', index=False)
-    df = df[~df['PostalCode'].isnull()]
-
-    return df
-
 def augment_doe(df):
-    doe = pandas.read_csv('Most-Recent-Cohorts-Institution.zip', low_memory=False)
-    doe = doe[['INSTNM', 'ADM_RATE_ALL', 'SAT_AVG_ALL', 'COSTT4_A', 'ZIP']]
-    doe.columns = ['School', 'AdmissionRate', 'SAT', 'Cost', 'PostalCode']
+    doe = pandas.read_csv('data/Most-Recent-Cohorts-Institution.zip', low_memory=False)
+    doe = doe[['INSTNM', 'ADM_RATE_ALL', 'SAT_AVG_ALL', 'COSTT4_A', 'LONGITUDE', 'LATITUDE']]
+    doe.columns = ['School', 'AdmissionRate', 'SAT', 'Cost', 'Longitude', 'Latitude']
 
-    doe = doe[~doe['PostalCode'].isnull()]
+    doe['SchoolKey'] = doe['School'].map(name_key)
+    df['SchoolKey'] = df['School'].map(name_key)
+    df = df.merge(doe, on='SchoolKey', how='left', suffixes=['', '_y'])
+    df = df.drop(['SchoolKey', 'School_y'], axis=1)
 
-    df = df.merge(doe, on='PostalCode', how='left')
     return df
 
-def get_wikidata(wikidata_id):
-    """Get some extra data from Wikidata for a given Wikidata ID
-    """
-    time.sleep(.5)
-    url = f"https://www.wikidata.org/w/rest.php/wikibase/v0/entities/items/{wikidata_id}/statements"
-    data = requests.get(url).json()
-    return {
-        "WikidataID": wikidata_id,
-        "PostalCode": pick(data, "P281"),
-        "StudentCount": pick(data, "P2196"),
-        "AdmissionRate": pick(data, "P5822"),
-        "Location": pick(data, "P625")
-    }
-
-def pick(data, prop):
-    """Get a Wikidata property value out of the Wikidata JSON
-    """
-    if prop in data and len(data[prop]) > 0:
-        first = data[prop][0]
-        prop_type = first["property"]["data-type"]
-        if prop_type == "string":
-            return first["value"]["content"]
-        elif prop_type == "quantity":
-            return first["value"]["content"]["amount"]
-        elif prop_type == "globe-coordinate":
-            content = first["value"]["content"]
-            return f"{content['longitude']},{content['latitude']}"
-    return None
+def name_key(s):
+    s = s.upper()
+    s = re.sub(r'\[.+?\]', '', s)
+    s = re.sub(r'[^A-Z]', '', s)
+    s = ''.join(sorted(s))
+    return s
 
 if __name__ == "__main__":
     main()
